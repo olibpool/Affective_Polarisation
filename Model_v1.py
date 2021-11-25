686b74bea98cf0a224349b4048635a5d502b2ca5
olibpool <68647683+olibpool@users.noreply.github.com>
2021-11-25 10:38:43 +0000

    Created Log saving capabilities

--- Model_v1.py
+++ Model_v1.py
@@ -1,24 +1,39 @@
 import copy
-
 import numpy as np
 import random
+import datetime
 
 # Set properties of model:
 
 N = 100  # Population size
-g = 5  # Number of groups
+g = 4  # Number of groups
 qi = 1  # In-group success probability - default = 1
 qo = 0.6  # Out-group success probability 0.6
 Bi = 1  # In-group benefit - default = 1
 Bo = 2  # Out-group Benefit
-sigma = 1 / (N - N / 10)  # To keep N*sigma ~  1
+sigma = 1 / N  # To keep N*sigma ~  1
 p = 1  # Polarisation
-trials = 10000  # Number of trials
+trials = 1000  # Number of trials
 
-print(f"Model properties: \n\nPopulation size, N: {N} \nNumber of groups, g: {g} \n"
-      f"In-group success probability, qi: {qi} \nOut-group success probability, qo: {qo} \n"
-      f"In-group benefit, Bi {Bi} \nOut-group benefit, Bo: {Bo} \n"
-      f"Strength of selection, sigma: {sigma}\nPolarisation p: {p} \nNumber of trails, trials: {trials}")
+parameters = f"Model properties: \n\nPopulation size, N: {N} \nNumber of groups, g: {g} \nIn-group success " \
+             f"probability, qi: {qi} \nOut-group success probability, qo: {qo} \nIn-group benefit, " \
+             f"Bi {Bi} \nOut-group benefit, Bo: {Bo} \nStrength of selection, sigma: {sigma}\nPolarisation p: {p} " \
+             f"\nNumber of trails, trials: {trials} \n"
+
+print(parameters)
+
+# Save to log? True = Save
+log = True
+
+# Create log file
+if log:
+    filename = "Logs/Model " + str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+    f = open(filename, "a")
+    f.write(parameters)
+    f.write("\n")
+    f.write("\nThese parameters give the following p-values: \n")
+    f.close()
 
 # Initial population saved as a dictionary with value 1 suggesting polarised.
 # member index: [polarisation (pi), group number]
@@ -37,12 +52,8 @@ for j in range(0, N, int(N / g)):
             start_population[k][1] = g_num
 
 # Adjacency matrix
-# Configured to just create a simple connected graph for now.
-adj = [[1 if i != j else 0 for i in range(N)] for j in range(N)]
-
-# Choose a random member of the population and flip their polarisation
-initial = random.randrange(N)
-start_population[initial][0] = 0
+# Configured to just create a simple connected graph for now. Also array is used for effciency purposes.
+adj = np.array([[1 if i != j else 0 for i in range(N)] for j in range(N)])
 
 
 # Expected fitness
@@ -84,41 +95,64 @@ def pbar(group, neigh):
 
 pol_flips = 0  # Number of times that the population finishes with 0 polarisation
 
-for __ in range(trials):
-    initial = random.randrange(N)
-    population = copy.deepcopy(start_population)
-    population[initial][0] = 0
+for tests in range(100000):
+    results = []
 
-    for _ in range(100 * N):
-        # Select i and calculate properties
-        i = random.randrange(N)
-        i_neighbours = neighbours(i)
-        i_group = population[i][1]
-        i_pobar = pbar(i_group, i_neighbours)
+    for counter in range(trials):
+        # Choose a random member of the population and flip their polarisation
+        initial = random.randrange(N)
+        population = copy.deepcopy(start_population)
+        population[initial][0] = 0
 
-        # Select j and calculate properties
-        j = random.choice(i_neighbours)
-        j_neighbours = neighbours(j)
-        j_group = population[j][1]
-        j_pobar = pbar(j_group, j_neighbours)
+        for _ in range(200 * N):
+            # Select i and calculate properties
+            i = random.randrange(N)
+            i_pol = population[i][0]
+            i_neighbours = neighbours(i)
 
-        # Expected fitness for i and j
-        wi = wi_func(population[i][0], i_pobar)
-        wj = wi_func(population[j][0], j_pobar)
+            # Select j and calculate properties
+            j = random.choice(i_neighbours)
+            j_pol = population[j][0]
 
-        # Probability that i will copy j strategy
-        prob = prob_function(wi, wj)
+            # Only continue if the polarisations are different
+            if i_pol != j_pol:
 
-        if random.random() < prob:
-            population[i][0] = population[j][0]
+                i_group = population[i][1]
+                i_pobar = pbar(i_group, i_neighbours)
 
-        # Check whether the absorbing state of zero polarisation or maximal polarisation has been reached
-        first = population[0][0]
-        if first in [0, 1] and all(flag == first for [flag, _] in population.values()):
-            if first == 0:
-                pol_flips += 1
-            break
+                j_neighbours = neighbours(j)
+                j_group = population[j][1]
+                j_pobar = pbar(j_group, j_neighbours)
 
-print()
-print("The probability that zero polarisation takes over is:")
-print(pol_flips / trials)
+                # Expected fitness for i and j
+                wi = wi_func(population[i][0], i_pobar)
+                wj = wi_func(population[j][0], j_pobar)
+
+                # Probability that i will copy j strategy
+                prob = prob_function(wi, wj)
+
+                if random.random() < prob:
+                    population[i][0] = population[j][0]
+
+                # Check whether the absorbing state of zero polarisation or maximal polarisation has been reached
+                first = population[0][0]
+                if first in [0, 1] and all(flag == first for [flag, _] in population.values()):
+                    if first == 0:
+                        pol_flips += 1
+                    break
+
+    print()
+    print("The probability that zero polarisation takes over is:")
+    print(pol_flips / trials)
+    results.append(pol_flips / trials)
+
+    if log:
+        with open(filename, "a") as f:
+            f.write(str(pol_flips / trials) + "\n")
+
+avg = sum(results) / len(results)
+
+if log:
+    f.write(f"\n The average p-val was: {avg} \n\n The results were: \n")
+    f.write(str(results))
+    f.close()
